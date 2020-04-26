2f0c4b09379a83176f12550ab0ed097ecc21734d
Pamela Christian <pamelac08@gmail.com>
2020-04-26 15:18:56 -0500

    adding dc remote connection to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // setup the code to connect Node to MySQL.
 var mysql = require("mysql");
 
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Racer2016",
     database: "burgers_db"
   });
-
+};
 
 // Make connection.
 connection.connect(function(err) {
